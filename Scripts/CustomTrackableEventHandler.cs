21bcee9b71c79c04540667b82d860b76bffc6e4a
Agapi Davradou <adavradou@gmail.com>
2020-04-11 13:49:35 +0300

    Add a countdown timer and a failure text when timer goes to 0.

--- Scripts/CustomTrackableEventHandler.cs
+++ Scripts/CustomTrackableEventHandler.cs
@@ -40,6 +40,7 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 	
 	//Initialize variables.	
 	public TextMeshProUGUI congratsText; //	Variable for the 3D text displaying when game is completed.	
+	public TextMeshProUGUI failedText; //	Variable for the 3D text displaying when game is completed.	
 	public AudioSource audioData; //Sound effect that plays when game is completed.
 	public AudioClip clip;
 	bool correctSequence = false;
@@ -47,8 +48,11 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 	
 	public Text attemptsText; 
 	public Text shuffleText;
+	public Text countdownText;
 
 	int totalTrackablesNum; //Number of QR codes in this game. Change it respectively.	
+	
+	float targetTime = 10f; //TO FIX: 	Make it public to change it for every scene
 	
 
 	int attempts = -1; //Keeps the number of user's attempts till game completion.
@@ -71,6 +75,7 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
     protected virtual void Start()
     {		
 		congratsText.enabled = false;
+		failedText.enabled = false;
 		shuffleText.enabled = false;
 		audioData = (AudioSource)gameObject.AddComponent<AudioSource>();
 		totalTrackablesNum = getTrackableNumber();
@@ -172,110 +177,127 @@ public class CustomTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 		StateManager sm = TrackerManager.Instance.GetStateManager();
 		IList<TrackableBehaviour> activeTrackables = (IList<TrackableBehaviour>) sm.GetActiveTrackableBehaviours();
 		
-		if (gameCompleted == false) { 			
+		if (targetTime <= 0.0f)
+		{
+			failedText.enabled = true;
+			countdownText.enabled = false; 
+			Debug.Log("TIME ENDED");
+			Invoke("GameComplete", 2f); //Wait 2 seconds before calling the GameComplete method.
+			//timeEnded();
+			//TO FIX: Later will call the timeEnded method, which will lead to another scene, showing the correct answer.
+		}
+		else{
+			if (gameCompleted == false) { 			
 			
-			if(activeTrackables.Count == totalTrackablesNum) //If all the QR codes of the game are currently tracked. 
-			{
-				int index = 0;
+				targetTime -= Time.deltaTime;
+				int intTime = (int) targetTime;
+				countdownText.GetComponent<Text>().text =  "Time remaining: " + intTime.ToString(); //Update the remaining time on the screen.		
 				
-				// Iterate through the list of active trackables
-				foreach (TrackableBehaviour tb in activeTrackables) {					
-				
-					x_pos[index] = tb.transform.position.x; //Stores all the x positions (not in a sorted order).
+				if(activeTrackables.Count == totalTrackablesNum) //If all the QR codes of the game are currently tracked. 
+				{
+					int index = 0;
 					
-					//Store the card order of the sequence, depending on the Image Target's name.
-					if (tb.TrackableName == "QR1_scaled"){ 
-						sequenceOrder[index] = 1;		
+					// Iterate through the list of active trackables
+					foreach (TrackableBehaviour tb in activeTrackables) {					
+					
+						x_pos[index] = tb.transform.position.x; //Stores all the x positions (not in a sorted order).
+						
+						//Store the card order of the sequence, depending on the Image Target's name.
+						if (tb.TrackableName == "QR1_scaled"){ 
+							sequenceOrder[index] = 1;		
+						}
+						
+						if (tb.TrackableName == "QR2_scaled"){ 
+							sequenceOrder[index] = 2;		
+						}					
+						
+						if (tb.TrackableName == "QR3_scaled"){ 
+							sequenceOrder[index] = 3;
+						}	
+
+						if (tb.TrackableName == "QR4_scaled"){ 
+							sequenceOrder[index] = 4;		
+						}
+						
+						if (tb.TrackableName == "QR5_scaled"){ 
+							sequenceOrder[index] = 5;		
+						}					
+						
+						if (tb.TrackableName == "QR6_scaled"){ 
+							sequenceOrder[index] = 6;
+						}		
+						
+						if (tb.TrackableName == "QR7_scaled"){ 
+							sequenceOrder[index] = 7;		
+						}					
+						
+						if (tb.TrackableName == "QR8_scaled"){ 
+							sequenceOrder[index] = 8;
+						}	
+						
+						if (tb.TrackableName == "QR9_scaled"){ 
+							sequenceOrder[index] = 9;		
+						}					
+						
+						if (tb.TrackableName == "QR10_scaled"){ 
+							sequenceOrder[index] = 10;
+						}	
+					
+						index = index + 1;
+						
 					}
 					
-					if (tb.TrackableName == "QR2_scaled"){ 
-						sequenceOrder[index] = 2;		
-					}					
+					sortArrays(x_pos, sequenceOrder);
 					
-					if (tb.TrackableName == "QR3_scaled"){ 
-						sequenceOrder[index] = 3;
-					}	
+					order = "";
+					for (int i = 0; i < sequenceOrder.Length; i++) 
+						order = order + sequenceOrder[i].ToString(); //Convert int array to string variable.
+					
+					correctSequence = isSorted(sequenceOrder); //Checks if sequence is correct.				
+		
 
-					if (tb.TrackableName == "QR4_scaled"){ 
-						sequenceOrder[index] = 4;		
-					}
-					
-					if (tb.TrackableName == "QR5_scaled"){ 
-						sequenceOrder[index] = 5;		
-					}					
-					
-					if (tb.TrackableName == "QR6_scaled"){ 
-						sequenceOrder[index] = 6;
-					}		
-					
-					if (tb.TrackableName == "QR7_scaled"){ 
-						sequenceOrder[index] = 7;		
-					}					
-					
-					if (tb.TrackableName == "QR8_scaled"){ 
-						sequenceOrder[index] = 8;
-					}	
-					
-					if (tb.TrackableName == "QR9_scaled"){ 
-						sequenceOrder[index] = 9;		
-					}					
-					
-					if (tb.TrackableName == "QR10_scaled"){ 
-						sequenceOrder[index] = 10;
-					}	
-				
-					index = index + 1;
-					
-				}
-				
-				sortArrays(x_pos, sequenceOrder);
-				
-				order = "";
-				for (int i = 0; i < sequenceOrder.Length; i++) 
-					order = order + sequenceOrder[i].ToString(); //Convert int array to string variable.
-				
-				correctSequence = isSorted(sequenceOrder); //Checks if sequence is correct.				
-	
+					if (correctSequence == true){ 	
 
-				if (correctSequence == true){ 	
-
-					Debug.Log("correctSequence");
+						Debug.Log("correctSequence");
 
 
-					if (attempts == -1)
-					{
-						Debug.Log("attempts are 0:");
-						//Visualize a text to shiffle the cards
-						//shuffleText. enabled = true;
-						StartCoroutine(ShowMessage(3));
+						if (attempts == -1)
+						{
+							Debug.Log("attempts are 0:");
+							//Visualize a text to shiffle the cards
+							//shuffleText. enabled = true;
+							StartCoroutine(ShowMessage(3));
+						}
+						else{
+							Debug.Log("attempts are NOT 0:" + attempts);
+							attempts = attempts+1;
+							attemptsText.GetComponent<Text>().text = "Attempts: " + attempts.ToString(); //Update the attempts count on the screen.
+							gameCompleted = true;
+							
+							Debug.Log("CONGRATULATIONS! YOU WON!! Attempts: " + attempts + " Final Sequence: " + order);
+							
+							congratsText.enabled = true; //Display congratulations 3D shaded text. 
+							AudioSource.PlayClipAtPoint(clip, transform.position); 	//Play epic effect audio.				
+								
+							Invoke("GameComplete", 3f); //Wait 3 seconds before calling the GameComplete method.								
+						}					
+
 					}
 					else{
-						Debug.Log("attempts are NOT 0:" + attempts);
-						attempts = attempts+1;
-						attemptsText.GetComponent<Text>().text = attempts.ToString(); //Update the attempts count on the screen.
-						gameCompleted = true;
 						
-						Debug.Log("CONGRATULATIONS! YOU WON!! Attempts: " + attempts + " Final Sequence: " + order);
-						
-						congratsText.enabled = true; //Display congratulations 3D shaded text. 
-						AudioSource.PlayClipAtPoint(clip, transform.position); 	//Play epic effect audio.				
-							
-						Invoke("GameComplete", 3f); //Wait 3 seconds before calling the GameComplete method.								
+						if (order != previousOrder){ //Checks if the user has changed the order of the cards.
+							attempts = attempts+1;
+							previousOrder = order; 		
+							attemptsText.GetComponent<Text>().text = "Attempts: " + attempts.ToString(); //Update the attempts count on the screen.
+							Debug.Log("..... Attempts: " + attempts + "......" + " Current order: " + order);					
+						}
 					}					
-
-				}
-				else{
-					
-					if (order != previousOrder){ //Checks if the user has changed the order of the cards.
-						attempts = attempts+1;
-						previousOrder = order; 		
-						attemptsText.GetComponent<Text>().text = attempts.ToString(); //Update the attempts count on the screen.
-						Debug.Log("..... Attempts: " + attempts + "......" + " Current order: " + order);					
-					}
-				}					
-			}			
-		}
+				}			
+			}
+		}			
 	}
+		
+
 	
 	
 	public IEnumerator ShowMessage (float delay)
